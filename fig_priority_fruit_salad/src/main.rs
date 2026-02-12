d71b2906ba0dcc0e9a4527488a9c10fc09821d69
becool2018 <djessup72@yahoo.com>
2026-02-12 16:44:56 -0700

    adding pagerank proj and finished fig_priority project

---
+++ fig_priority_fruit_salad/src/main.rs
@@ -0,0 +1,26 @@
+use rand::{seq::SliceRandom, thread_rng};
+use std::collections::BTreeSet;
+
+fn main() {
+    let fruits = vec![
+        "apple", "banana", "orange", "grape", "kiwi", "mango", "peach", "pear",
+    ];
+    let amounts = [1, 3, 5, 7, 9];
+
+    let mut rng = thread_rng();
+
+    for amount in amounts.iter() {
+        let mut fruit_set = BTreeSet::new();
+        let mut shuffled_fruits = fruits.clone();
+        shuffled_fruits.shuffle(&mut rng);
+
+        for fruit in shuffled_fruits {
+            fruit_set.insert(fruit);
+            if fruit_set.len() == *amount {
+                break;
+            }
+        }
+
+        println!("{}: {:?}", amount, fruit_set);
+    }
+}
---
+++ pagerank_for_sports_data/src/main.rs
@@ -0,0 +1,60 @@
+use rand::{seq::SliceRandom, thread_rng};
+use std::cmp::Ord;
+use std::collections::BinaryHeap;
+
+#[derive(Eq, PartialEq)]
+
+enum Fruit {
+    Fig,
+    Other(String), // We can represent other fruits as a string, but they will be considered less than Fig in the ordering.
+}
+
+// We define Figs as the highest priority by implementing Ord such that Fig is greater than any other fruit.
+impl Ord for Fruit {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match (self, other) {
+            (Fruit::Fig, Fruit::Fig) => std::cmp::Ordering::Equal,
+            (Fruit::Fig, Fruit::Other(_)) => std::cmp::Ordering::Greater,
+            (Fruit::Other(_), Fruit::Fig) => std::cmp::Ordering::Less,
+            (Fruit::Other(_), Fruit::Other(_)) => std::cmp::Ordering::Equal, // All other fruits are considered equal in priority.
+        }
+    }
+}
+
+impl PartialOrd for Fruit {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn generated_fruit_salad() -> BinaryHeap<Fruit> {
+    let mut rng = thread_rng();
+    let fruits = vec![
+        "Apple", "Orange", "Pear", "Peach", "Banana", "Fig", "Fig", "Fig",
+    ];
+    let mut fruit_salad = BinaryHeap::new();
+
+    let mut figs_count = 0;
+    while figs_count < 2 {
+        let fruit = fruits.choose(&mut rng).unwrap();
+        if *fruit == "Fig" {
+            figs_count += 1;
+            fruit_salad.push(Fruit::Fig);
+        } else {
+            fruit_salad.push(Fruit::Other(fruit.to_string()));
+        }
+    }
+
+    fruit_salad
+}
+
+fn main() {
+    let fruit_salad = generated_fruit_salad();
+    println!("Generated Fruit Salad:");
+    for fruit in fruit_salad {
+        match fruit {
+            Fruit::Fig => println!("Fig"),
+            Fruit::Other(name) => println!("{}", name),
+        }
+    }
+}
