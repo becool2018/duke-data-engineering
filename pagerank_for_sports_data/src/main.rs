541c702092fdf458d1b86cf8eb666a8ec35e3181
becool2018 <djessup72@yahoo.com>
2026-02-12 17:17:25 -0700

    page_rank finsihed

--- pagerank_for_sports_data/src/main.rs
+++ pagerank_for_sports_data/src/main.rs
@@ -1,60 +1,63 @@
-use rand::{seq::SliceRandom, thread_rng};
-use std::cmp::Ord;
-use std::collections::BinaryHeap;
+use textwrap::fill;
 
-#[derive(Eq, PartialEq)]
-
-enum Fruit {
-    Fig,
-    Other(String), // We can represent other fruits as a string, but they will be considered less than Fig in the ordering.
+struct PageRank {
+    damping: f64,
+    iterations: usize,
 }
 
-// We define Figs as the highest priority by implementing Ord such that Fig is greater than any other fruit.
-impl Ord for Fruit {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        match (self, other) {
-            (Fruit::Fig, Fruit::Fig) => std::cmp::Ordering::Equal,
-            (Fruit::Fig, Fruit::Other(_)) => std::cmp::Ordering::Greater,
-            (Fruit::Other(_), Fruit::Fig) => std::cmp::Ordering::Less,
-            (Fruit::Other(_), Fruit::Other(_)) => std::cmp::Ordering::Equal, // All other fruits are considered equal in priority.
-        }
-    }
-}
-
-impl PartialOrd for Fruit {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-fn generated_fruit_salad() -> BinaryHeap<Fruit> {
-    let mut rng = thread_rng();
-    let fruits = vec![
-        "Apple", "Orange", "Pear", "Peach", "Banana", "Fig", "Fig", "Fig",
-    ];
-    let mut fruit_salad = BinaryHeap::new();
-
-    let mut figs_count = 0;
-    while figs_count < 2 {
-        let fruit = fruits.choose(&mut rng).unwrap();
-        if *fruit == "Fig" {
-            figs_count += 1;
-            fruit_salad.push(Fruit::Fig);
-        } else {
-            fruit_salad.push(Fruit::Other(fruit.to_string()));
+impl PageRank {
+    fn new(damping: f64, iterations: usize) -> Self {
+        Self {
+            damping,
+            iterations,
         }
     }
 
-    fruit_salad
+    fn rank(&self, graph: &Vec<Vec<usize>>) -> Vec<f64> {
+        let n = graph.len();
+        let mut ranks = vec![1.0 / n as f64; n];
+
+        for _ in 0..self.iterations {
+            // A new vector to hold the updated ranks after this iteration
+            let mut new_ranks = vec![0.0; n];
+
+            // Iterate over each node and its edges in the graph.
+            for (node, edges) in graph.iter().enumerate() {
+                let contribution = ranks[node] / edges.len() as f64;
+
+                // Distribute the rank contribution to each connected node
+                for &edge in edges {
+                    new_ranks[edge] += contribution;
+                }
+            }
+
+            for rank in &mut new_ranks {
+                *rank = self.damping * *rank + (1.0 - self.damping) / n as f64;
+            }
+            ranks = new_ranks;
+        }
+
+        ranks
+    }
 }
 
 fn main() {
-    let fruit_salad = generated_fruit_salad();
-    println!("Generated Fruit Salad:");
-    for fruit in fruit_salad {
-        match fruit {
-            Fruit::Fig => println!("Fig"),
-            Fruit::Other(name) => println!("{}", name),
-        }
+    let graph = vec![
+        vec![1, 2], // Node 0 is connected to nodes 1 and 2
+        vec![0],    // Node 1 is connected to node 0
+        vec![0, 3], // Node 2 is connected to nodes 0 and 3
+        vec![0],    // Node 3 is connected to node 0
+        vec![0, 1],
+    ];
+
+    let names = vec!["ESPN", "NFL", "NBA", "UFC", "MLB"];
+
+    let pagerank = PageRank::new(0.85, 100);
+    let ranks = pagerank.rank(&graph);
+    for (i, rank) in ranks.iter().enumerate() {
+        println!("The PageRank of {} is {:.4}", names[i], rank);
     }
+
+    let explanation = "PageRank is a link analysis algorithm used by Google Search that uses the structure of the web to determine the importance of individual pages. It works by counting the number and quality of links to a page to determine a rough estimate of how important the website is. The underlying assumption is that more important websites are likely to receive more links from other websites.";
+    println!("\n{}", fill(explanation, 80));
 }
